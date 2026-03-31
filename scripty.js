bd96389afc4d009f814ca8c88d63d03280556d5c
risavavra-cmyk <risavavra@gmail.com>
2026-03-31 12:10:33 +0200

    trinact

---
+++ scripty.js
@@ -0,0 +1,5 @@
+function changeImage(event, src) {
+        document.getElementById('mainImage').src = src;
+        document.querySelectorAll('.thumbnail').forEach(thumb => thumb.classList.remove('active'));
+        event.target.classList.add('active');
+    }
